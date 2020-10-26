76564b2a2f3bd6302c9c55db74c78bb1027e95de
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-10-26 09:09:23 +0200

    Implement vertex insertion

--- pbfs/bag.cpp
+++ pbfs/bag.cpp
@@ -1,1 +1,29 @@
 #include "bag.h"
+
+Bag::Bag()
+{
+
+}
+
+//void Bag::insert(std::unique_ptr<Pennant> pennant)
+void Bag::insert(int vertex)
+{
+	auto pennant = std::make_unique<Pennant>(vertex);
+
+	for (int i = 0; i < pennants.size(); ++i)
+	{
+		if (!pennants[i])
+		{
+			pennants[i] = std::move(pennant);
+			return;
+		}
+		else
+		{
+			//pennant->merge(pennants[i].release());	
+			pennant->merge(pennants[i]);	// destructively reads the pennants[i] and sets it to null
+		}
+	}
+
+	// If there were no empty slot, append it
+	pennants.push_back(std::move(pennant));
+}
--- pbfs/bag.h
+++ pbfs/bag.h
@@ -1,10 +1,24 @@
 #ifndef BAG_H
 #define BAG_H
 
+#include "pennant.h"
+
+#include <vector>
+#include <memory>
 
 class Bag
 {
+public:
+	Bag();
 
+	// A convenience method for inserting a single vertex into a bag
+	void insert(int vertex);
+
+	//void insert(std::unique_ptr<Pennant> pennant);
+
+private:
+	std::vector<std::unique_ptr<Pennant>> pennants;
+	//std::vector<Pennant> pennants;
 };	// Bag
 
 #endif	// BAG_H
