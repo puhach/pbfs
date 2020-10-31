e2629264825ff1ff9f6871e175996eeb8c286631
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-10-31 07:46:21 +0200

    Return the size of the pennant

--- pbfs/pennant.h
+++ pbfs/pennant.h
@@ -1,13 +1,19 @@
 #ifndef PENNANT_H
 #define PENNANT_H
 
+#include <cstddef>
+
 class Pennant
 {
 public:
 	Pennant(int vertex)
-		: vertex(vertex) {}
+		: vertex(vertex), size(1) {}
+
+	constexpr std::size_t getSize() const { return size; }
+
 private:
 	int vertex;
+	std::size_t size;
 };	// Pennant
 
 #endif // PENNANT_H
